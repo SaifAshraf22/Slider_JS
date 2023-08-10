e6db8cdea99dfc548e6b4e14b22e73a93d7bafec
Saif_Ashraf <112732757+SaifAshraf22@users.noreply.github.com>
2023-08-10 15:37:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let i = 0;
+let j = 4;
+
+const dots = document.querySelectorAll(".dot-container button");
+const images = document.querySelectorAll(".image-container img");
+
+//next previous buttons
+function next() {
+  document.getElementById("content" + (i + 1)).classList.remove("active");
+  i = (j + i + 1) % j;
+  document.getElementById("content" + (i + 1)).classList.add("active");
+  indicator(i + 1);
+}
+
+function prev() {
+  document.getElementById("content" + (i + 1)).classList.remove("active");
+  i = (j + i - 1) % j;
+  document.getElementById("content" + (i + 1)).classList.add("active");
+  indicator(i + 1);
+}
+
+//active indicator
+function indicator(num) {
+  dots.forEach(function (dot) {
+    dot.style.backgroundColor = "transparent";
+  });
+  document.querySelector(
+    ".dot-container button:nth-child(" + num + ")"
+  ).style.backgroundColor = "#076bb8";
+}
+
+//active dot
+function dot(index) {
+  images.forEach(function (image) {
+    image.classList.remove("active");
+  });
+  document.getElementById("content" + index).classList.add("active");
+
+  i = index - 1;
+  indicator(index);
+}
